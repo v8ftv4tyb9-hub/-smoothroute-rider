07e7ab0919d37afbaa4b14baba953aa918826f27
v8ftv4tyb9-hub <v8ftv4tyb9@privaterelay.appleid.com>
2026-08-15 20:03:56 -0700

    Clear legacy SmoothRoute caches and retire old service worker

--- sw.js
+++ sw.js
@@ -1,40 +1,19 @@
-const CACHE_NAME = 'smoothroute-rider-v2';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
+const CACHE_NAME = 'smoothroute-rider-v06-cleanup';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.registration.unregister();
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
